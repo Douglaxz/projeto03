bd099617de705241c223f38cfa66fd8edbdfeafd
Douglas Amaral <douglaxz@hotmail.com>
2023-01-17 18:08:57 -0300

    correções na tabela

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from galeria.models import Fotografia
+from django.contrib import messages
 
-def index(request):  
+def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect('login')
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
     return render(request,'galeria/index.html',{"cards":fotografias})
 
@@ -11,6 +15,9 @@ def imagem(request, foto_id):
     return render(request,'galeria/imagem.html',{"fotografia":fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect('login')    
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
     if "buscar" in request.GET:
         nome_a_buscar = request.GET['buscar']
